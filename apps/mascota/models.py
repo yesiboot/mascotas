96ab34ed7c7d493771c59cd7724eaddf69cdbe75
reboot <yesireboot@gmail.com>
2017-09-18 21:18:27 -0500

    correccion de commit esta es la appweb mascotas falta el tema de validaciones

---
+++ apps/mascota/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from apps.adopcion.models import Persona
+
+# Create your models here.
+
+
+class Vacuna(models.Model):
+	nombre = models.CharField(max_length=50)
+
+	def __str__(self):
+		return '{}'.format(self.nombre)
+
+	
+class Mascota(models.Model):
+	nombre = models.CharField(max_length=10)
+	sexo = models.CharField(max_length=10)
+	edad_aproximada = models.IntegerField()
+	fecha_rescate = models.DateField() 
+	persona = models.ForeignKey(Persona, null=True, blank=True, on_delete=models.CASCADE)
+	vacuna = models.ManyToManyField(Vacuna, blank=True)
